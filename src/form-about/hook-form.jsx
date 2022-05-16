e3d87e5f9c9969185d0d47165bc80dd5f98cc342
EFSZ\zhouming.wang <zhouming.wang@ecoflow.com>
2022-05-16 17:18:31 +0800

    feat: 添加react-hook-form，好像无法在vite上使用？

---
+++ src/form-about/hook-form.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+// import { useForm, Controller } from "react-hook-form";
+
+// export default function () {
+//   const { register, handleSubmit } = useForm();
+//   const onSubmit = data => console.log(data);
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       <input type="text" {...register("name")} />
+//       <input type="password" {...register("password")} />
+//       <input type="submit" value={'login'} />
+//     </form>
+//   );
+// }
--- src/form-about/index.jsx
+++ src/form-about/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import OriginForm from "./origin-form";
+// import HookForm from "./hook-form";
 
 export default function () {
   return <OriginForm />;
+  // return <HookForm />;  // react-hook-form无法在vite上使用？
 }
